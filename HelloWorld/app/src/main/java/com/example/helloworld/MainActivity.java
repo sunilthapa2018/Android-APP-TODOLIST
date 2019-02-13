f756fcfc203c022774a5470beb36f1051bd5c576
Sunil Thapa <luck_sunilthapa@yahoo.com>
2019-02-13 17:37:08 +0545

    Task 6 completed

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtCount = (TextView) findViewById(R.id.txtCount);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, "Hello! This is a Toast Message", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void increaseCount(View view) {
+        int countValue = 0;
+        countValue = Integer.parseInt(txtCount.getText().toString());
+        countValue++;
+        txtCount.setText(String.valueOf(countValue));
+
     }
 }

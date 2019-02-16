5bca018709d426e66a2cae21ba6b8e25f8278602
unknown <luck_sunilthapa@yahoo.com>
2019-02-16 20:02:22 +0545

    color corrected

--- Week1/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Week1/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtCount;
+    Button btnZero;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtCount = (TextView) findViewById(R.id.txtCount);
+        btnZero = (Button) findViewById(R.id.btnZero);
     }
 
     public void showToast(View view) {
@@ -21,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increaseCount(View view) {
-        int countValue = 0;
-        countValue = Integer.parseInt(txtCount.getText().toString());
+        btnZero.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
+        int countValue = Integer.parseInt(txtCount.getText().toString());
         countValue++;
         txtCount.setText(String.valueOf(countValue));
+    }
 
+    public void putZero(View view) {
+        txtCount.setText(""+0);
+        btnZero.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
     }
 }
